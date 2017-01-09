829665a95a55225707339df16afcd8fbf049352c
Shane Smith <ssmith@nerdery.com>
2017-01-08 23:05:31 -0600

    Update to bundles.

--- scripts/install_vundle.sh
+++ scripts/install_vundle.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # install if it doesn't already exist
-if [ ! -d "${HOME}/.vim/bundle/Vundle.vim" ]; then
+#if [ ! -d "${HOME}/.vim/bundle/Vundle.vim" ]; then
 	echo "";
 	echo -n "installing vundle ...";
 	mkdir -p ~/.vim/bundle > /dev/null 2>&1;
 	git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim > /dev/null 2>&1;
 	echo " done";
-else
-	echo "vundle is already installed."
-fi
+#else
+#	echo "vundle is already installed."
+#fi
