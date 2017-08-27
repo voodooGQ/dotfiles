7a76d4cc730f51b6fbd08de8d5f28a1288d7374f
Shane Smith <voodoogq@gmail.com>
2017-08-27 18:34:40 -0500

    Removing setup.sh and updating README

--- setup.sh
+++
@@ -1,9 +0,0 @@
-# !/bin/sh
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-stow bash git vim tmux
-$DIR/scripts/./install_vundle.sh
-echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-";
-echo "";
-echo "Don't forget to load vim and run :PluginInstall";
-echo "Also run: source ~/.bash_profile";
