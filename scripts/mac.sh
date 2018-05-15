73f62a9c9c74ea2eba78cbad13a8fc06a2fd978e
Shane Smith <shane.smith@LB-SS1.local>
2018-05-15 15:37:05 -0700

    Adding zsh to the mac script

--- scripts/mac.sh
+++ scripts/mac.sh
@@ -4,7 +4,7 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install packages
-brew install nvim python2 python node
+brew install nvim python2 python node zsh
 
 # Upgrade pip3
 pip3 install --upgrade pip setuptools wheel
@@ -13,3 +13,10 @@ pip3 install --upgrade pip setuptools wheel
 pip3 install neovim
 pip install neovim
 npm install -g neovim
+
+# Install base16-shell
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+
+# Install Oh-My-ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s /bin/zsh
