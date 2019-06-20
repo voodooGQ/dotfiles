4728ffcc34ec13107605365530d336d5a19dca03
Shane Smith <voodoogq@gmail.com>
2019-06-20 09:36:09 -0500

    WTF config. Updates to htoprc. Added work display screenlayouts

---
+++ system/.screenlayout/3-screen.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 3784x1656 --rotate normal --output DP1 --off --output DP2-1 --primary --mode 2560x1080 --pos 1224x1192 --rotate normal --output DP2-2 --off --output DP2-3 --mode 2560x1080 --pos 0x0 --rotate right --output HDMI2 --off --output HDMI1 --off --output DP2 --off
