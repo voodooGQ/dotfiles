0a2f084ea0450024fa8a2534355fe5aa244afed9
Shane Smith <voodoogq@gmail.com>
2019-07-19 21:03:55 -0500

    Updating screen layout files

--- system/.screenlayout/3-screen.sh
+++ system/.screenlayout/3-screen.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 3640x2640 --rotate normal --output DP1 --off --output DP2-1 --primary --mode 2560x1080 --pos 1080x2272 --rotate normal --output DP2-2 --off --output DP2-3 --mode 2560x1080 --pos 0x1080 --rotate right --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+xrandr --output VIRTUAL1 --off \
+       --output eDP1 --mode 1920x1080 --pos 3640x2640 --rotate normal \
+       --output DP1 --off \
+       --output DP2-1 --primary --mode 2560x1080 --pos 1080x2272 --rotate normal \
+       --output DP2-2 --off --output DP2-3 --mode 2560x1080 --pos 0x1080 --rotate right \
+       --output HDMI2 --off --output HDMI1 --off --output DP2 --off
