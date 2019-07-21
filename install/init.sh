198325ebc7d4948ab8e13c3ac8fb239474813bb7
Shane Smith <voodoogq@gmail.com>
2019-07-21 00:48:41 -0500

    Updates

--- install/init.sh
+++ install/init.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-#  Neovim
-sudo apt-add-repository ppa:neovim-ppa/stable
-
-# Etcher Deb
-echo "deb https://deb.etcher.io stable etcher" | sudo tee /etc/apt/sources.list.d/balena-etcher.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
-
 sudo apt-get update
 
 # Library depedendencies
