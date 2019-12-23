11f7eeaaeef4b764b0283fbba0c04ac23630f9c6
Shane Smith <voodoogq@gmail.com>
2019-12-23 09:41:35 -0600

    Changes

--- feh/.config/feh/randomwallpaper.sh
+++ feh/.config/feh/randomwallpaper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true
 do
-  feh --randomize --bg-scale --no-xinerama ~/Pictures/Wallpapers/*
+  feh --no-fehbg --bg-scale --recursive --randomize "${HOME}/Pictures/Wallpapers"
   # Every 10 minutes
   sleep 600
 done
