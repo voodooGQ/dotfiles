9c0f63fd6590e6d374ede3ac4cfdd564815afa37
Shane Smith <voodoogq@gmail.com>
2020-04-02 07:01:07 -0500

    Update pre-restore

--- system/.screenlayout/3-screen-horizontal.sh
+++ system/.screenlayout/3-screen-horizontal.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 5120x488 --rotate normal --output DP1 --off --output DP2-1 --primary --mode 2560x1080 --pos 2560x40 --rotate normal --output DP2-2 --off --output DP2-3 --mode 2560x1080 --pos 0x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 5360x432 --rotate normal --output DP1 --off --output DP2-1 --mode 2560x1080 --pos 0x0 --rotate normal --output DP2-2 --primary --mode 2560x1080 --pos 2672x248 --rotate normal --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
