094a05daad055793103a6ec6a8ccf4b114a608b5
Shane Smith <voodoogq@gmail.com>
2020-08-27 09:05:33 -0500

    Updates

--- scripts/aws/bucket-removal-script.py
+++ scripts/aws/bucket-removal-script.py
@@ -1,6 +1,7 @@
 import boto3
 import re
 
+
 def delete_buckets():
     next_token = None
     s3 = boto3.client('s3', region_name='us-east-1')
@@ -8,7 +9,6 @@ def delete_buckets():
 
     for bucket in buckets:
         bucket_name = bucket.get('Name')
-        if not re.match(r'.+msp.+', bucket_name): continue
         print('Deleting bucket %s' % bucket_name)
         res = boto3.resource('s3')
         buck = res.Bucket(bucket_name)
