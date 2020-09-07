7e0ad718833366b9a277dc5d8de7727c44c43753
Shane Smith <voodoogq@gmail.com>
2020-09-07 13:28:09 -0500

    Adding kajabi sit

---
+++ system/.screenlayout/3-screen-sit-kajabi.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-2-1 --mode 2560x1080 --pos 0x0 --rotate normal --output DP-2-2 --off --output DP-2-3 --mode 2560x1080 --pos 2592x0 --rotate normal --output eDP-1 --primary --mode 1920x1080 --pos 1512x1128 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --off
